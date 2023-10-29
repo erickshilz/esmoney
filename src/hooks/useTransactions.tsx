c8ffabddebe18ff8ce780e9c8e77f67cb55320ad
Erick Shil <erickeshil@gmail.com>
2023-10-29 11:53:17 -0300

    pasta hooks criada e hook de transações adicionado

---
+++ src/hooks/useTransactions.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { api } from '../services/api';
+
+interface Transaction {
+  id: number;
+  title: string;
+  type: string;
+  amount: number;
+  category: string;
+  createdAt: string;
+}
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+interface TransactionsProviderProps {
+  children: React.ReactNode;
+}
+
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
+
+const TransactionsContext = React.createContext<TransactionsContextData>(
+  {} as TransactionsContextData,
+);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = React.useState<Transaction[]>([]);
+
+  React.useEffect(() => {
+    api
+      .get('transactions')
+      .then((response) => setTransactions(response.data.transactions));
+  }, []);
+
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+    setTransactions([...transactions, transaction]);
+  }
+
+  return (
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
+
+export function useTransactions() {
+  const context = React.useContext(TransactionsContext);
+
+  return context;
+}
