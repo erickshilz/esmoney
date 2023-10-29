e9f3405b40bfc4547273ea83e21cab72c6672eb8
Erick Shil <erickeshil@gmail.com>
2023-10-29 12:05:08 -0300

    app.tsx e main.tsx atualizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Modal from 'react-modal';
+import { Header } from './components/Header';
+import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { GlobalStyle } from './styles/global';
+
+Modal.setAppElement('#root');
+
+const App = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    React.useState(false);
+
+  function handleOpenNewTransactinoModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactinoModal() {
+    setIsNewTransactionModalOpen(false);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactinoModal} />
 
-export default App
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactinoModal}
+      />
+
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { Model, createServer } from 'miragejs';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        // { DEPÓSITOS PRÉ-CADASTRADOS
+        //   id: 1,
+        //   title: 'Freelancer de Website',
+        //   type: 'deposit',
+        //   category: 'Desenvolvimento',
+        //   amount: 6000,
+        //   createdAt: new Date('2023-02-12 09:00:00'),
+        // },
+        // {
+        //   id: 2,
+        //   title: 'Aluguel',
+        //   type: 'withdraw',
+        //   category: 'Casa',
+        //   amount: 1100,
+        //   createdAt: new Date('2023-02-14 20:00:00'),
+        // },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
+  },
+});
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(<App />);
